594844ff5992b52264d1754a7bf762d71637f87b
aryansinghh1 <aryansinghkush2522@gmail.com>
2026-02-23 10:23:05 +0530

    abstract

---
+++ Abstract_01.java
@@ -0,0 +1,21 @@
+abstract class Animal {
+  // Abstract method (does not have a body)
+  abstract void animalSound();
+  // Regular method
+  public void sleep() {
+    System.out.println("Zzz");
+  }
+}
+class Pig extends Animal {
+  public void animalSound() {
+    // The body of animalSound() is provided here
+    System.out.println("The pig says: wee wee");
+  }
+}
+public class Abstract_01 {
+    public static void main(String[] args) {
+    Pig myPig = new Pig(); // Create a Pig object
+    myPig.animalSound();
+    myPig.sleep();
+  }
+}
