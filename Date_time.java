e632c366d671bd1a4317a4fc5212dcfb8c3eb5f4
aryansinghh1 <aryansinghkush2522@gmail.com>
2026-02-27 09:55:27 +0530

    data and time

---
+++ Date_time.java
@@ -0,0 +1,17 @@
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+public class Date_time {
+    public static void main(String[] args) {
+        LocalDate myObj = LocalDate.now(); // Create a date object
+        System.out.println(myObj); // Display the current date
+
+        LocalTime myObj1 = LocalTime.now();
+        System.out.println(myObj1);
+
+        LocalDateTime myObj2 = LocalDateTime.now();
+        System.out.println(myObj2);
+
+    }
+}
