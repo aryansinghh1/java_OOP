4878d39bf358f33c55c7ff938d568a5736c24dea
aryansinghh1 <aryansinghkush2522@gmail.com>
2026-02-27 09:42:30 +0530

    enum

---
+++ Enum_01.java
@@ -0,0 +1,13 @@
+
+public class Enum_01 {
+    enum Level {
+        LOW,
+        MEDIUM,
+        HIGH
+    }
+
+    public static void main(String[] args) {
+        Level myVar = Level.MEDIUM;
+        System.out.println(myVar);
+    }
+}
